5dee37ec1960d543eabad52e373551c09569edc7
Sebastian <sebastian.studniczek@outlook.com>
2021-01-08 12:29:05 +0100

    Clean up pages and views

---
+++ Source/BlazorUI/Pages/OrchestralBriefcase/OrchestralBriefcaseEdit.razor.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading.Tasks;
+using EOrchestralBriefcase.Application.Dtos.OrchestralBriefcases;
+using EOrchestralBriefcase.BlazorUI.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace EOrchestralBriefcase.BlazorUI.Pages.OrchestralBriefcase
+{
+    public partial class OrchestralBriefcaseEdit
+    {
+        [Inject]
+        public IOrchestralBriefcasesService OrchestralBriefcasesService { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
+        public string PageHeaderText { get; set; }
+        public string SubmitButtonText { get; set; }
+        public string Message { get; set; }
+        public OrchestralBriefcaseUpdateDto UpdateDto { get; set; }
+            = new OrchestralBriefcaseUpdateDto();
+        public OrchestralBriefcaseCreateDto CreateDto { get; set; }
+            = new OrchestralBriefcaseCreateDto();
+
+        protected override async Task OnParametersSetAsync()
+        {
+            if (Id != 0)
+            {
+                PageHeaderText = "Edytuj teczkę";
+                SubmitButtonText = "Zapisz";
+                var readDto = await OrchestralBriefcasesService.GetByIdAsync(Id);
+
+                UpdateDto.Id = readDto.Id;
+                UpdateDto.Name = readDto.Name;
+            }
+            else
+            {
+                PageHeaderText = "Dodaj nową teczkę";
+                SubmitButtonText = "Dodaj";
+            }
+        }
+
+        private async Task HandleValidSubmit()
+        {
+            if (Id != 0)
+            {
+                await OrchestralBriefcasesService
+                    .UpdateAsync(Id, UpdateDto);
+                Message = "Teczka została zaktualizowana.";
+            }
+            else
+            {
+                await OrchestralBriefcasesService
+                    .CreateAsync(CreateDto);
+                Message = "Teczka została dodana.";
+            }
+        }
+
+        private void HandleAbort()
+        {
+            NavigationManager.NavigateTo($"orchestralbriefcases/{Id}");
+        }
+    }
+}
---
+++ Source/BlazorUI/Pages/OrchestralPiece/OrchestralPieceList.razor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EOrchestralBriefcase.Application.Dtos.OrchestralPieces;
+using EOrchestralBriefcase.BlazorUI.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace EOrchestralBriefcase.BlazorUI.Pages.OrchestralPiece
+{
+    public partial class OrchestralPieceList
+    {
+        [Inject]
+        public IOrchestralBriefcasesService OrchestralBriefcasesService { get; set; }
+        [Inject]
+        public IOrchestralPiecesService OrchestralPiecesService { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
+
+        public string OrchestralBriefcaseName { get; set; }
+        public IEnumerable<OrchestralPieceReadDto> OrchestralPieces { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            OrchestralPieces = await OrchestralBriefcasesService
+                .GetAllOrchestralPiecesForBriefcase(Id);
+            var orchestralBriefcase = await OrchestralBriefcasesService
+                .GetByIdAsync(Id);
+            OrchestralBriefcaseName = orchestralBriefcase.Name;
+        }
+
+        protected async Task OnDeleteOrchestralPieceAsync(int orchestralPieceId)
+        {
+            await OrchestralPiecesService.DeleteOrchestralPieceById(orchestralPieceId);
+            OrchestralPieces = await OrchestralBriefcasesService.GetAllOrchestralPiecesForBriefcase(Id);
+        }
+    }
+}
