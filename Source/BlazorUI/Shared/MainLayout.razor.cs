cc30975fecece9237749c9b05fe3cf39f0d282ec
Sebastian <sebastian.studniczek@outlook.com>
2021-01-08 12:31:35 +0100

    Refactor shared components

---
+++ Source/BlazorUI/Shared/MainLayout.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EOrchestralBriefcase.BlazorUI.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace EOrchestralBriefcase.BlazorUI.Shared
+{
+    public partial class MainLayout 
+    {
+        [Inject]
+        public IOrchestralBriefcasesService OrchestralBriefcaseseService { get; set; }
+
+        public IEnumerable<OrchestralBriefcasesService> OrchestralBriefcases { get; set; }
+    }
+}
